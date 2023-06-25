e1ea57ca4e1425fb0dd2cc9f019559c40d8ef94e
ahmedasad236 <ahmedasad236@gmail.com>
2023-06-25 18:43:57 +0300

    Add the basic pages and its styles with css modules, and some images for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="/pricing"
+          element={<Pricing />}
+        />
+
+        <Route
+          path="/product"
+          element={<Product />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
