99dd3288cab268069f3c415b33f66804fe13f5e8
ahmedasad236 <ahmedasad236@gmail.com>
2023-06-29 12:58:08 +0300

    Implement Fake login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -10,68 +10,71 @@ import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            index
-            element={<HomePage />}
-          />
-          <Route
-            path="/app"
-            element={<AppLayout />}
-          >
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
             <Route
               index
-              element={
-                <Navigate
-                  replace
-                  to="cities"
-                />
-              }
+              element={<HomePage />}
             />
             <Route
-              path="cities"
-              element={<CityList />}
-            />
+              path="/app"
+              element={<AppLayout />}
+            >
+              <Route
+                index
+                element={
+                  <Navigate
+                    replace
+                    to="cities"
+                  />
+                }
+              />
+              <Route
+                path="cities"
+                element={<CityList />}
+              />
+              <Route
+                path="cities/:id"
+                element={<City />}
+              />
+              <Route
+                path="countries"
+                element={<CountryList />}
+              />
+              <Route
+                path="form"
+                element={<Form />}
+              />
+            </Route>
             <Route
-              path="cities/:id"
-              element={<City />}
+              path="/pricing"
+              element={<Pricing />}
             />
-            <Route
-              path="countries"
-              element={<CountryList />}
-            />
-            <Route
-              path="form"
-              element={<Form />}
-            />
-          </Route>
-          <Route
-            path="/pricing"
-            element={<Pricing />}
-          />
 
-          <Route
-            path="/product"
-            element={<Product />}
-          />
+            <Route
+              path="/product"
+              element={<Product />}
+            />
 
-          <Route
-            path="/login"
-            element={<Login />}
-          />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
 
-          <Route
-            path="*"
-            element={<PageNotFound />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+            <Route
+              path="*"
+              element={<PageNotFound />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,10 @@
 import { createContext, useContext, useReducer } from 'react';
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz'
+};
 
 const initialState = {
   user: null,
@@ -21,10 +27,13 @@ function reducer(state, action) {
 const AuthContext = createContext();
 function AuthProvider({ children }) {
   const [{ user, isAuth }, dispatch] = useReducer(reducer, initialState);
-  function login(email, password) {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: 'auth/login', payload: FAKE_USER });
+  }
 
   function logout() {
-    dispatch({ type: 'logout' });
+    dispatch({ type: 'auth/logout' });
   }
 
   return (
