ab9d003684c72e8c7dd53b27a457de61170aa818
ahmedasad236 <ahmedasad236@gmail.com>
2023-07-04 06:35:21 +0300

    Apply lazy loading on pages, to decrease the bundle size and improve the app performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,88 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Homepage';
-import Pricing from './pages/Pricing';
-import Product from './pages/Product';
-import PageNotFound from './pages/PageNotFound';
-import Login from './pages/Login';
-import AppLayout from './pages/AppLayout';
+import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/AuthContext';
+
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
-import { CitiesProvider } from './contexts/CitiesContext';
-import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './pages/ProtectedRoute';
+import SpinnerFullPage from './components/SpinnerFullPage';
+
+const HomePage = lazy(() => import('./pages/Homepage'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const Product = lazy(() => import('./pages/Product'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+const Login = lazy(() => import('./pages/Login'));
+const AppLayout = lazy(() => import('./pages/AppLayout'));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route
-              index
-              element={<HomePage />}
-            />
-            <Route
-              path="/app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
               <Route
                 index
+                element={<HomePage />}
+              />
+              <Route
+                path="/app"
                 element={
-                  <Navigate
-                    replace
-                    to="cities"
-                  />
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
                 }
-              />
+              >
+                <Route
+                  index
+                  element={
+                    <Navigate
+                      replace
+                      to="cities"
+                    />
+                  }
+                />
+                <Route
+                  path="cities"
+                  element={<CityList />}
+                />
+                <Route
+                  path="cities/:id"
+                  element={<City />}
+                />
+                <Route
+                  path="countries"
+                  element={<CountryList />}
+                />
+                <Route
+                  path="form"
+                  element={<Form />}
+                />
+              </Route>
               <Route
-                path="cities"
-                element={<CityList />}
+                path="/pricing"
+                element={<Pricing />}
               />
-              <Route
-                path="cities/:id"
-                element={<City />}
-              />
-              <Route
-                path="countries"
-                element={<CountryList />}
-              />
-              <Route
-                path="form"
-                element={<Form />}
-              />
-            </Route>
-            <Route
-              path="/pricing"
-              element={<Pricing />}
-            />
 
-            <Route
-              path="/product"
-              element={<Product />}
-            />
+              <Route
+                path="/product"
+                element={<Product />}
+              />
 
-            <Route
-              path="/login"
-              element={<Login />}
-            />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
 
-            <Route
-              path="*"
-              element={<PageNotFound />}
-            />
-          </Routes>
+              <Route
+                path="*"
+                element={<PageNotFound />}
+              />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
